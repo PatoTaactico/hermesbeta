5613a7434ea8773e8761880c4b00424e7353d4a1
Estebjack-2004git config --global user.name Estebjack-2004git config --global user.name Estebjack-2004 <ortizvasquezjuanesteban15@gmail.com>
2025-06-28 16:17:22 -0500

    he arreglado la visualizacuion de la preventana de mantenimiento

--- vistas/modulos/Mantenimiento.php
+++ vistas/modulos/Mantenimiento.php
@@ -15,6 +15,7 @@
       </div>
     </div>
   </section>
+  <!-- fin de encabezado -->
 
   <!-- Inicio de la tabla -->
   <section class="content">
@@ -43,23 +44,23 @@
                     foreach ($mantenimientos as $key => $value) {
                       echo '
                         <tr>
-                          <td>' . $value["equipo_id"] . '</td>
-                          <td>' . $value["numero_serie"] . '</td>
-                          <td>' . $value["etiqueta"] . '</td>
-                          <td>' . $value["descripcion"] . '</td>
-                          <td>
-                            <div class="btn-group">
-                              <button title="Ver detalles" class="btn btn-default btn-sm btnVerDetalles" data-id="' . $value["equipo_id"] . '" data-toggle="modal" data-target="#modalVerDetalles">
-                                <i class="fas fa-eye"></i>
-                              </button>
-                              <button title="Editar mantenimiento" class="btn btn-default btn-sm btnEditarMantenimiento" data-id="' . $value["Id_mantenimiento"] . '" data-toggle="modal" data-target="#modalEditarMantenimiento">
-                                <i class="fas fa-edit"></i>
-                              </button>
-                              <button title="Solicitudes asociadas" class="btn btn-default btn-sm btnSolicitudesMantenimiento" data-id="' . $value["equipo_id"] . '">
-                                <i class="fas fa-laptop"></i>
-                              </button>
-                            </div>
-                          </td>
+                            <td>' . $value["equipo_id"] . '</td>
+                            <td>' . $value["numero_serie"] . '</td>
+                            <td>' . $value["etiqueta"] . '</td>
+                            <td>' . $value["descripcion"] . '</td>
+                            <td>
+                                <div class="btn-group">
+                                    <button title="Finalizar mantenimiento" class="btn btn-success btn-sm btnFinalizarMantenimiento" data-id="' . $value["Id_mantenimiento"] . '">
+                                        <i class="fas fa-check"></i>
+                                    </button>
+                                    <button title="Ver detalles" class="btn btn-default btn-sm btnVerDetalles" data-id="' . $value["equipo_id"] . '" data-toggle="modal" data-target="#modalVerDetalles">
+                                        <i class="fas fa-eye"></i>
+                                    </button>
+                                    <button title="Editar mantenimiento" class="btn btn-default btn-sm btnEditarMantenimiento" data-id="' . $value["Id_mantenimiento"] . '" data-toggle="modal" data-target="#modalEditarMantenimiento">
+                                        <i class="fas fa-edit"></i>
+                                    </button>
+                                </div>
+                            </td>
                         </tr>';
                     }
                   } else {
@@ -74,6 +75,71 @@
       </div>
     </div>
   </section>
+  <!-- Fin de la tabla -->
+
+  <!-- Modal Finalizar Mantenimiento -->
+  <div class="modal fade" id="modalFinalizarMantenimiento">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header bg-info">
+          <h5 class="modal-title text-white" id="modalFinalizarMantenimientoLabel">Detalles del Mantenimiento</h5>
+          <button type="button" class="close text-white" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body p-0">
+          <div class="p-3">
+            <!-- ... (contenido original igual) ... -->
+          </div>
+
+          <div class="p-3 bg-light">
+            <h5 class="border-bottom pb-2">Estado del Mantenimiento</h5>
+            <form id="formFinalizarMantenimiento" method="post">
+              <input type="hidden" id="equipoId" name="equipoId">
+
+              <div class="form-group">
+                <label>Nivel de Gravedad:</label>
+                <div class="d-flex">
+                  <div class="custom-control custom-radio mr-4">
+                    <input type="radio" id="sinNovedad" name="gravedad" value="ninguno" class="custom-control-input">
+                    <label class="custom-control-label" for="sinNovedad">Sin novedad</label>
+                  </div>
+                  <div class="custom-control custom-radio mr-4">
+                    <input type="radio" id="problemaLeve" name="gravedad" value="leve" class="custom-control-input">
+                    <label class="custom-control-label" for="problemaLeve">Problema leve</label>
+                  </div>
+                  <div class="custom-control custom-radio">
+                    <input type="radio" id="problemaGrave" name="gravedad" value="grave" class="custom-control-input">
+                    <label class="custom-control-label" for="problemaGrave">Problema grave</label>
+                  </div>
+                </div>
+              </div>
+
+              <div class="form-group">
+                <label for="tipoMante" class="form-label">Tipo de Mantenimiento:</label>
+                <select name="tipoMantenimiento" id="tipoMante" class="form-control custom-select">
+                  <option value="" disabled selected>Seleccione el tipo de mantenimiento</option>
+                  <option value="preventivo">Preventivo</option>
+                  <option value="correctivo">Correctivo</option>
+                </select>
+              </div>
+
+              <div class="form-group">
+                <label for="descripcionProblema">Descripción del problema:</label>
+                <textarea class="form-control" id="descripcionProblema" name="detalles" rows="3" required></textarea>
+              </div>
+
+              <div class="text-center mt-4">
+                <button type="submit" class="btn btn-info px-4" id="btnGuardarMantenimiento">
+                  <i class="fas fa-check-circle mr-2"></i>Marcar como Finalizado
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <!-- Modal Ver Detalles -->
   <div class="modal fade" id="modalVerDetalles">
